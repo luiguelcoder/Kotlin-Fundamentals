d358bfd3ac580aeec4bc598c2d13650e1b1f2eed
Luis Miguel Mantilla <lmmantilla9350@gmail.com>
2020-05-19 23:44:59 -0400

    Add splash activity

---
+++ EventsApp/app/src/main/java/nearshorecode/com/eventsapp/ui/activities/SplashActivity.kt
@@ -0,0 +1,13 @@
+package nearshorecode.com.eventsapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nearshorecode.com.eventsapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
