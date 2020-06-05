5dca07176e66c6013921302eee7dd600e408a5f5
Luis Miguel Mantilla <lmmantilla9350@gmail.com>
2020-06-04 21:55:49 -0400

    Add an animation in the splash activity

--- EventsApp/app/src/main/java/nearshorecode/com/eventsapp/view/ui/activities/SplashActivity.kt
+++ EventsApp/app/src/main/java/nearshorecode/com/eventsapp/view/ui/activities/SplashActivity.kt
@@ -1,7 +1,11 @@
 package nearshorecode.com.eventsapp.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
 import nearshorecode.com.eventsapp.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +13,25 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        mainLogo.startAnimation(animation)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationRepeat(animation: Animation?) {
+                print("Start")
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+                print("Start")
+            }
+        });
     }
 }
